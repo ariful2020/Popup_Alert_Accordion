ce6517fdc3d9b6e4605f475ab6e56b78339a9b4d
ariful2020 <programmerariful@gmail.com>
2022-02-04 22:30:04 +0600

    Update File

--- js/app.js
+++ js/app.js
@@ -54,8 +54,13 @@ const accordion_title = document.querySelectorAll('.accourdion-title')
  
 accordion_title.forEach(item => {
     item.addEventListener('click',function(){
+ 
+        accordion_title.forEach(items => {
+            items.classList.remove('active');
+            items.nextElementSibling.style.height = "0px";
+        })
+
         item.classList.toggle('active')
-        
         if(item.classList.contains("active")){
             item.nextElementSibling.style.height = item.nextElementSibling.scrollHeight + "px"
         }else{
